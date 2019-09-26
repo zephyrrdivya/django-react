3a4491670d7d04b4b8f31bc049b530f9f4cb7642
zephyrrdivya <zephyrrdivya@gmail.com>
2019-09-26 10:16:59 +0545

    added owner to clients

--- new/clients/models.py
+++ new/clients/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Client(models.Model):
@@ -6,4 +7,5 @@ class Client(models.Model):
     email = models.EmailField(unique=True)
     address = models.CharField(max_length=300)
     contact = models.CharField(max_length=16, unique=True)
+    owner = models.ForeignKey(User, related_name="clients", on_delete=models.CASCADE, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
